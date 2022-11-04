f881d4c4f986e9855408b703ba9fdcf4dab6e870
Leon-Ali <lomaliyakubov@gmail.com>
2022-11-04 11:09:08 +0300

    Setup db connection for postgres

--- todolist/todolist/settings.py
+++ todolist/todolist/settings.py
@@ -87,8 +87,12 @@ WSGI_APPLICATION = 'todolist.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': env.str('POSTGRES_DB'),
+        'USER': env.str('POSTGRES_USER'),
+        'PASSWORD': env.str('POSTGRES_PASSWORD'),
+        'HOST': env.str('POSTGRES_HOST', default='127.0.0.1'),
+        'PORT': '5432',
     }
 }
 
