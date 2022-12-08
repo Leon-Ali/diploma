f2581e4e07895694b41c168ea00af3fd85ee038e
Leon-Ali <lomaliyakubov@gmail.com>
2022-12-08 07:36:22 +0300

    Add all new stuff

--- todolist/todolist/settings.py
+++ todolist/todolist/settings.py
@@ -54,6 +54,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'corsheaders',
     'social_django',
+    'django_filters',
 ]
 
 MIDDLEWARE = [
@@ -164,5 +165,6 @@ SOCIAL_AUTH_LOGIN_ERROR_URL = '/login-error/'
 # REST_FRAMEWORK
 
 REST_FRAMEWORK = {
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination'
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
 }
